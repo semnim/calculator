7c33390e940089456a91af10a37e0833939d46fa
Semjon Nirmann <semnir@uni-bremen.de>
2022-04-11 14:48:16 +0200

    implement basic functions

--- script.js
+++ script.js
@@ -1,61 +1,106 @@
-let display = document.querySelector('.display');
-let displayValue = document.querySelector(".display").textContent;
+let display = document.querySelector(".display--main");
+let memoryDisplay = document.querySelector(".display--upper")
 let numButtons = document.querySelectorAll(".num--button");
 let operatorButtons = document.querySelectorAll(".operator--button");
-let operation = null;
 let equalsButton = document.querySelector(".equals--button");
-let operandA = "";
-let operandB = 0;
+let clearButton = document.querySelector(".clear");
+let deleteButton = document.querySelector(".delete");
+const operation = {
+  sign: "",
+  x: "",
+  y: "",
+  result: "",
+};
+const operators = ["+", "-", "×", "÷"];
+
+clearButton.addEventListener("click", () => {
+     memoryDisplay.textContent = "Cleared";
+     display.textContent = "0";
+});
+deleteButton.addEventListener("click", () => {
+    console.log("log");
+    display.textContent = display.textContent.slice(0, -1);
+});
+
+document.addEventListener("keypress", (e) => {
+  switch (e.key) {
+    case "+":
+      document.getElementById("add").click();
+      break;
+    case "-":
+      document.getElementById("subtract").click();
+      break;
+    case "*":
+      document.getElementById("multiply").click();
+      break;
+    case "/":
+      document.getElementById("divide").click();
+      break;
+    case "=":
+    case "Enter":
+      document.getElementById("equal").click();
+      break;
+    default:
+      if (e.key >= 0 && e.key <= 9) {
+        document.getElementById(e.key).click();
+      }
+  }
+});
 
 equalsButton.addEventListener("click", () => {
-    display.textContent = operate(operation, parseInt(operandA), operandB);
-    displayValue = display.textContent;
-    checkClearScreen();
-    operation = null;
-});
-numButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        if (operation !== null) {
-            operandB = parseInt(button.textContent);
-            display.textContent = operandB;
-        } else if (displayValue === "0" || display.textContent == "÷" || display.textContent == "×" || display.textContent == "+" || display.textContent == "-") {
-            display.textContent = button.textContent;
-            operandA = display.textContent;
-        } else {
-            display.textContent += button.textContent;
-            operandA += button.textContent;
-            console.log(operandA);
-        }
-        displayValue = display.textContent;
-        console.log("Disp: " + displayValue);
-        checkClearScreen();
-    });
+  if (operation.sign === "") {
+      return;
+  }
+  operation.y = display.textContent;
+  if (operation.sign === "÷" && operation.y === "0") {
+      memoryDisplay.textContent = "Stop screwing around.";
+      return;
+  }
+  operation.result = operate(operation.sign, parseInt(operation.x), parseInt(operation.y));
+  memoryDisplay.textContent += ` ${operation.y} =`;
+  display.textContent = operation.result;
+  operation.sign = "";
 });
 
-operatorButtons.forEach(button => {
-    button.addEventListener("click", () => {
-        operation = button.textContent;
-        display.textContent = operation;
-        displayValue = display.textContent;
-        console.log(displayValue);
-        checkClearScreen();
-    });
-});
-function checkClearScreen() {
-    if (displayValue.length == 7) {
-        display.textContent = "";
+function updateDisplay(number) {
+    if (display.textContent === "0" || display.textContent === "") {
+        display.textContent = number;
+    } else {
+        display.textContent += number;
     }
 }
-function operate(operator, x, y) {
-    console.log(operator, x, y);
-    switch (operator) {
-        case '+':
-            return x + y;
-        case '-':
-            return x - y;
-        case '×':
-            return x * y;
-        case '÷':
-            return x / y;
+numButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    updateDisplay(button.textContent);
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    operation.sign = button.textContent;
+    updateMemory(operation.sign);
+  });
+});
+function updateMemory(operator) {
+    let lastChar = memoryDisplay.textContent.slice(-1);
+    
+    if (operators.includes(lastChar)) {
+        memoryDisplay.textContent = memoryDisplay.textContent.replace(/.$/, operator);
+    } else {
+        operation.x = display.textContent;
+        memoryDisplay.textContent = `${display.textContent} ${operator}`;
     }
+    display.textContent = "";
+}
+function operate(operator, x, y) {
+  switch (operator) {
+    case "+":
+      return x + y;
+    case "-":
+      return x - y;
+    case "×":
+      return x * y;
+    case "÷":
+      return x / y;
+  }
 }
